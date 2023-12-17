3685d00d4110e41a7a160e30a1360a40fa4adb73
Katsiaryna <katsiaryna.n@gmail.com>
2023-12-17 11:21:12 +0300

    feat: add popup for network errors

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -2,10 +2,12 @@ import { DocExplorer } from './components/DocExplorer/DocExplorer';
 import { InputApi } from './components/InputEndpoint/InputEndpoint';
 import { Editor } from './components/Editor/Editor';
 import styles from './Main.module.scss';
+import { ErrorPopUp } from './components/ErrorPopUp/ErrorPopUp';
 
 export function Main() {
   return (
     <main className={styles.main}>
+      <ErrorPopUp />
       <InputApi />
       <DocExplorer />
       <Editor />
