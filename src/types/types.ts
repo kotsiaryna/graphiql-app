619842262011e21061d8e85d3e90c107d3875fde
amberlynn <amberlynn364@gmail.com>
2023-12-19 20:00:45 +0300

    refactor: minor changes

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,9 @@
+import * as yup from 'yup';
+import { userCredentialsSchema } from '../utils/userCredentialsSchema';
+
+export interface ValidFormData
+  extends yup.InferType<typeof userCredentialsSchema> {}
+
 export interface AuthorData {
   name: string;
   githubName: string;
