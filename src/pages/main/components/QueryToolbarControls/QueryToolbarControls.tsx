b9ea1e631a421abf886698f6806a81e09ccd10cb
@fedko-zhenja <fedkozhenja@gmail.com>
2023-12-23 17:48:38 +0400

    refactor: update control buttons

--- src/pages/main/components/QueryToolbarControls/QueryToolbarControls.tsx
+++ src/pages/main/components/QueryToolbarControls/QueryToolbarControls.tsx
@@ -1,3 +1,6 @@
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import IconButton from '@mui/material/IconButton';
 import { QueryToolbarControlsProps } from './types';
 import styles from './QueryToolbarControls.module.scss';
 
@@ -9,35 +12,38 @@ export function QueryToolbarControls({
 }: QueryToolbarControlsProps) {
   return (
     <div className={styles.queryToolbarControls}>
-      <button
-        className={
-          activeEditor === 'Variables'
-            ? styles.queryToolbarControls__activeButton
-            : styles.queryToolbarControls__button
-        }
-        type="button"
-        onClick={() => setActiveEditor('Variables')}
-      >
-        Variables
-      </button>
-      <button
-        className={
-          activeEditor === 'Headers'
-            ? styles.queryToolbarControls__activeButton
-            : styles.queryToolbarControls__button
-        }
-        type="button"
-        onClick={() => setActiveEditor('Headers')}
-      >
-        Headers
-      </button>
-      <button
-        className={styles.queryToolbarControls__button}
-        type="button"
-        onClick={changeEditorVisibility}
-      >
-        {isShowEditor ? 'Hide editor' : 'Show editor'}
-      </button>
+      <div className={styles.queryToolbarControls__editorButtons}>
+        <button
+          className={
+            activeEditor === 'Variables'
+              ? styles.queryToolbarControls__activeButton
+              : styles.queryToolbarControls__button
+          }
+          type="button"
+          onClick={() => setActiveEditor('Variables')}
+        >
+          Variables
+        </button>
+        <button
+          className={
+            activeEditor === 'Headers'
+              ? styles.queryToolbarControls__activeButton
+              : styles.queryToolbarControls__button
+          }
+          type="button"
+          onClick={() => setActiveEditor('Headers')}
+        >
+          Headers
+        </button>
+      </div>
+
+      <IconButton disableRipple edge="end" onClick={changeEditorVisibility}>
+        {isShowEditor ? (
+          <KeyboardArrowDownIcon fontSize="medium" />
+        ) : (
+          <KeyboardArrowUpIcon fontSize="medium" />
+        )}
+      </IconButton>
     </div>
   );
 }
