49260eb45e74b9d1507e47f978798ec0f23d00e0
Katsiaryna <katsiaryna.n@gmail.com>
2023-12-25 14:05:04 +0300

    feat: change error catching

--- src/redux/features/queryResponse/queryResponseSlice.ts
+++ src/redux/features/queryResponse/queryResponseSlice.ts
@@ -1,17 +1,38 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { QueryResponse } from '../../../types/types';
-import { getQuery } from '../../../api/getQuery';
+import axios from 'axios';
+import { QueryRequest, QueryResponse } from '../../../types/types';
 
-export const fetchQuery = createAsyncThunk('queryResponse/fetch', getQuery);
+export const fetchQuery = createAsyncThunk<
+  QueryResponse,
+  QueryRequest,
+  { rejectValue: string }
+>(
+  'queryResponse/fetch',
+  async ({ url, query, variables, headers }, { rejectWithValue }) => {
+    try {
+      const body = { query, variables };
+      const { data } = await axios.post<QueryResponse>(url, body, {
+        headers: { ...headers, 'Content-Type': 'application/json' },
+        validateStatus: (status) => status < 500,
+      });
+      return data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return rejectWithValue(error.message);
+      }
+      return rejectWithValue('An unexpected error occurred');
+    }
+  }
+);
 
 type InitStateType = {
   response: QueryResponse;
-  errorMessage: string | null;
+  errorMessage: string | undefined;
 };
 
 const initialState: InitStateType = {
   response: {},
-  errorMessage: null,
+  errorMessage: undefined,
 };
 
 export const queryResponseSlice = createSlice({
@@ -22,19 +43,19 @@ export const queryResponseSlice = createSlice({
       state.response = {};
     },
     deleteResponseError: (state) => {
-      state.errorMessage = null;
+      state.errorMessage = undefined;
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(fetchQuery.fulfilled, (state, action) => {
-      if (typeof action.payload === 'string') {
+    builder
+      .addCase(fetchQuery.fulfilled, (state, action) => {
+        state.response = action.payload;
+        state.errorMessage = undefined;
+      })
+      .addCase(fetchQuery.rejected, (state, action) => {
         state.response = {};
         state.errorMessage = action.payload;
-      } else {
-        state.response = action.payload;
-        state.errorMessage = null;
-      }
-    });
+      });
   },
 });
 
--- src/redux/features/schema/schemaSlice.ts
+++ src/redux/features/schema/schemaSlice.ts
@@ -1,14 +1,53 @@
+/* eslint-disable no-underscore-dangle */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { getSchema } from '../../../api/getSchema';
+import axios from 'axios';
+import { SchemaType } from '../../../types/types';
 
-export const fetchSchema = createAsyncThunk('schema/fetch', getSchema);
+const schemaQuery = {
+  query: `{
+    __schema {
+      types {
+        name
+        kind
+        description
+        fields {
+          name
+          description
+          type {
+            name
+            kind
+          }
+        }
+      }
+    }
+  }`,
+};
+
+export const fetchSchema = createAsyncThunk<
+  SchemaType['data']['__schema'],
+  string,
+  { rejectValue: string }
+>('schema/fetch', async (url, { rejectWithValue }) => {
+  try {
+    const { data } = await axios.post<SchemaType>(url, schemaQuery, {
+      headers: { 'Content-Type': 'application/json' },
+    });
+    return data.data.__schema;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return rejectWithValue(error.message);
+    }
+    return rejectWithValue('An unexpected error occurred');
+  }
+});
+
 interface SchemaResponse {
   data: object | null;
-  errorMessage: string | null;
+  errorMessage: string | undefined;
 }
 const initialState: SchemaResponse = {
   data: null,
-  errorMessage: null,
+  errorMessage: undefined,
 };
 
 export const schemaSlice = createSlice({
@@ -19,19 +58,19 @@ export const schemaSlice = createSlice({
       state.data = null;
     },
     deleteSchemaError: (state) => {
-      state.errorMessage = null;
+      state.errorMessage = undefined;
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(fetchSchema.fulfilled, (state, action) => {
-      if (typeof action.payload === 'string') {
-        state.errorMessage = action.payload;
-        state.data = null;
-      } else {
+    builder
+      .addCase(fetchSchema.fulfilled, (state, action) => {
         state.data = action.payload;
-        state.errorMessage = null;
-      }
-    });
+        state.errorMessage = undefined;
+      })
+      .addCase(fetchSchema.rejected, (state, action) => {
+        state.data = null;
+        state.errorMessage = action.payload;
+      });
   },
 });
 
