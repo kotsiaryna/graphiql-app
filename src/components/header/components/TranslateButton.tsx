1321ea2d2353873e5aafefcfe3b91dc7dc62ca90
Katsiaryna <katsiaryna.n@gmail.com>
2023-12-27 15:47:46 +0300

    feat: implement language changing

--- src/components/header/components/TranslateButton.tsx
+++ src/components/header/components/TranslateButton.tsx
@@ -2,15 +2,21 @@ import Stack from '@mui/material/Stack';
 import IconButton from '@mui/material/IconButton';
 import TranslateIcon from '@mui/icons-material/Translate';
 import Tooltip from '@mui/material/Tooltip';
-
 import * as React from 'react';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Fade from '@mui/material/Fade';
+import { LangContext } from '../../../context/langContext';
+import { Language } from '../../../types/types';
 
 export function TranslateButton() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
+
+  const enSelected = React.useRef(true);
+  const ruSelected = React.useRef(false);
+
+  const { setLang } = React.useContext(LangContext);
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -18,6 +24,27 @@ export function TranslateButton() {
 
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const changeLanguage: React.MouseEventHandler<HTMLElement> = (e) => {
+    const langValue = e.currentTarget.innerText.toLowerCase() as Language; // todo can we type this without assertion?
+    if (setLang) {
+      setLang(langValue);
+    }
+    switch (langValue) {
+      case 'en':
+        ruSelected.current = false;
+        enSelected.current = true;
+        break;
+      case 'ru':
+        ruSelected.current = true;
+        enSelected.current = false;
+        break;
+      default:
+        ruSelected.current = false;
+        enSelected.current = true;
+    }
+    handleClose();
   };
 
   return (
@@ -45,8 +72,12 @@ export function TranslateButton() {
         onClose={handleClose}
         TransitionComponent={Fade}
       >
-        <MenuItem onClick={handleClose}>EN</MenuItem>
-        <MenuItem onClick={handleClose}>RU</MenuItem>
+        <MenuItem selected={enSelected.current} onClick={changeLanguage}>
+          EN
+        </MenuItem>
+        <MenuItem selected={ruSelected.current} onClick={changeLanguage}>
+          RU
+        </MenuItem>
       </Menu>
     </div>
   );
