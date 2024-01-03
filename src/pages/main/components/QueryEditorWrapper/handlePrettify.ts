8ac72dbdbc3ec5f65bb8990befb91e06af492ea9
@fedko-zhenja <fedkozhenja@gmail.com>
2024-01-04 01:12:01 +0400

    feat: update handlePrettify

--- src/pages/main/components/QueryEditorWrapper/handlePrettify.ts
+++ src/pages/main/components/QueryEditorWrapper/handlePrettify.ts
@@ -1,50 +1,62 @@
 /* eslint-disable no-plusplus */
+const SPACE = ' ';
+const OPEN_BRACKET = '{';
+const CLOSE_BRACKET = '}';
+const COLON = ':';
+
 export function handlePrettify(query: string) {
   let openBracketCount = 0;
   let prettyQuery = '';
 
   if (query) {
-    const queryString = query.trim().replace(/(\r\n|\n|\r)/gm, '');
+    const queryString = query.trim().replace(/(\r\n|\n|\r)/gm, ''); // remove line breaks
 
     for (let i = 0; i < queryString.length; i++) {
       switch (queryString[i]) {
-        case '{': {
-          if (queryString[i + 1] === ' ') {
+        case OPEN_BRACKET: {
+          if (queryString[i + 1] === SPACE) {
             prettyQuery += `${queryString[i]}`;
             openBracketCount++;
           } else {
             openBracketCount++;
             prettyQuery += `${queryString[i]}\n`;
-            prettyQuery += ' '.repeat(openBracketCount * 2);
+            prettyQuery += SPACE.repeat(openBracketCount * 2);
           }
 
           break;
         }
 
-        case '}': {
-          prettyQuery += `\n${' '.repeat(openBracketCount * 2 - 2)}`;
+        case CLOSE_BRACKET: {
+          prettyQuery += `\n${SPACE.repeat(openBracketCount * 2 - 2)}`;
           openBracketCount--;
           prettyQuery += queryString[i];
 
           break;
         }
 
-        case ' ': {
+        case SPACE: {
           if (
-            queryString[i + 1] !== '}' &&
-            queryString[i + 1] !== ' ' &&
-            queryString[i + 1] !== '{'
+            queryString[i + 1] !== CLOSE_BRACKET &&
+            queryString[i + 1] !== SPACE &&
+            queryString[i + 1] !== OPEN_BRACKET
           ) {
             prettyQuery += `${queryString[i]}\n`;
-            prettyQuery += ' '.repeat(openBracketCount * 2);
+            prettyQuery += SPACE.repeat(openBracketCount * 2);
           }
 
-          if (queryString[i + 1] === '{') {
+          if (queryString[i + 1] === OPEN_BRACKET) {
             prettyQuery += queryString[i];
           }
 
-          if (queryString[i + 1] === ' ') {
-            prettyQuery += '';
+          break;
+        }
+
+        case COLON: {
+          if (queryString[i + 1] === SPACE) {
+            prettyQuery += queryString[i];
+            i++;
+          } else {
+            prettyQuery += queryString[i];
           }
 
           break;
