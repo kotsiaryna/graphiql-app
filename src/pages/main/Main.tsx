88347d16f87479ebfe85c32a99413cadb73b39fc
amberlynn <amberlynn364@gmail.com>
2024-01-07 20:40:41 +0300

    fix: fix redirect after token expired

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,10 +1,22 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import { DocExplorer } from './components/DocExplorer/DocExplorer';
 import { InputApi } from './components/InputEndpoint/InputEndpoint';
 import { Editor } from './components/Editor/Editor';
 import styles from './Main.module.scss';
 import { ErrorPopUp } from './components/ErrorPopUp/ErrorPopUp';
+import { auth } from '../../firebase';
+import { Path } from '../../router/types';
 
 export function Main() {
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loading) return;
+    if (!user) navigate(Path.Welcome);
+  }, [user, loading, navigate]);
   return (
     <main className={styles.main}>
       <InputApi />
